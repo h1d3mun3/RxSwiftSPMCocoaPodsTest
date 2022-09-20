c3dfba873b0b54af03f403825312908e53dd4b0b
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2022-09-20 10:59:51 +0900

    Initial Commit

---
+++ RxSwiftSPMCocoaPodsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftSPMCocoaPodsTest
+//
+//  Created by takahashi_hidemune on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
